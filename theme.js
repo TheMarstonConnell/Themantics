e410fee23c546cbfff7ad82aca941bf415036051
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2021-06-29 09:49:04 -0400

    Fixing view

--- theme.js
+++ theme.js
@@ -1,8 +1,3 @@
-const light_theme = {back1: '#eeeeee', back2: '#ffffff', content1: '#586e75', content2: '#657b83', content3: '#839496', content4: '#002b36'};
-const dark_theme = {};
-const solar_light = {back1: '#eee8d5', back2: '#fdf6e3', content1: '#586e75', content2: '#657b83', content3: '#839496', content4: '#002b36', yellow: '#b58900', orange: '#cb4b16', red: '#dc322f', magenta: '#d33682', violet: '#6c71c4', blue: '#268bd2', cyan: '#2aa198', green: '#859900'};
-const solar_dark = {back1: '#002b36', back2: '#073642', content1: '#586e75', content2: '#657b83', content3: '#839496', content4: '#93a1a1', yellow: '#b58900', orange: '#cb4b16', red: '#dc322f', magenta: '#d33682', violet: '#6c71c4', blue: '#268bd2', cyan: '#2aa198', green: '#859900'};
-
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
@@ -14,13 +9,21 @@ function setTheme(themeName) {
     }
 }
 
+
+
 function change_theme(){
     let drop = document.getElementById('color_picker_drop');
+    console.log("Switching theme!");
     setTheme(drop.value);
 }
 
 
 window.addEventListener('load', (event) => {
+
+    let drop = document.getElementById('color_picker_drop');
+    if(drop){
+
+    }
 
     let theme = localStorage.getItem('theme');
     if(theme == null){
